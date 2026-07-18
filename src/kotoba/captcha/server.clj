7d2c3a6786b7ab27dc898df011214f53d78812fa
Jun Kawasaki <jun784@gmail.com>
2026-07-18 19:29:21 +0900

    feat: make local solver server runnable

--- src/kotoba/captcha/server.clj
+++ src/kotoba/captcha/server.clj
@@ -1,7 +1,9 @@
 (ns kotoba.captcha.server
   "Optional local host. `run-server` is injected (for example http-kit), keeping
   the library dependency-light and the lifecycle explicit."
-  (:require [kotoba.captcha.http :as http]))
+  (:require [clojure.string :as str]
+            [kotoba.captcha.http :as http]
+            [kotoba.captcha.store :as store]))
 
 (defn start!
   [run-server config {:keys [port host] :or {port 8787 host "127.0.0.1"}}]
@@ -9,3 +11,14 @@
 
 (defn stop! [close-server]
   (when close-server (close-server)))
+
+(defn -main [& _]
+  (let [keys (some-> (System/getenv "KOTOBA_CAPTCHA_API_KEYS")
+                     (str/split #",") seq)
+        port (some-> (System/getenv "PORT") parse-long)
+        run-server (requiring-resolve 'org.httpkit.server/run-server)]
+    (when-not keys
+      (throw (ex-info "KOTOBA_CAPTCHA_API_KEYS is required" {:type :missing-api-keys})))
+    (start! run-server {:task-store (store/memory-store) :api-keys keys}
+            {:port (or port 8787) :host "127.0.0.1"})
+    (println "kotoba-captcha listening on 127.0.0.1:" (or port 8787))))
